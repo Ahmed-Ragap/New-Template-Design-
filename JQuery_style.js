535faceb1a7bd60c6a2efbf460a8c58c808abd32
Ahmed Ragap <Ahmed-Ragap>
2022-06-11 22:26:01 +0200

    update

---
+++ JQuery_style.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        autoplay:true,
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+                lazyLoad:true,
+         lazyContent:true,
+        autoplayHoverPause:true,
+        animateIn:true,
+        callbacks:true,
+        fallbackEasing:true,
+        rewind:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true,
+            },
+            600:{
+                items:1,
+                nav:true,
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop:true,
+               
+            }
+        }
+    });
+    // start AOS Animation
+    AOS.init();
+  });
